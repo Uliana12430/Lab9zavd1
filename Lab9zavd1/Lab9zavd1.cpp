a40db9fc6e3ae71be18eca79d5d7992d24dda1f0
ulian <ulian@JULY>
2024-11-18 00:06:30 +0200

    Добавьте файлы проекта.

---
+++ Lab9zavd1/Lab9zavd1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int start = 5, end = 50;
+    long long sum = 0;
+
+    for (int i = start; i <= end; ++i) {
+        sum += i * i * i; // Додаємо куб числа
+    }
+
+    cout << "Сума кубів всіх натуральних чисел від " << start << " до " << end << " дорівнює " << sum << "." << endl;
+
+    return 0;
+}
